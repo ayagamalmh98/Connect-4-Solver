c7fcd58ee0c96426e84636720fedf78212931bd6
ayagamalmh98 <48804613+ayagamalmh98@users.noreply.github.com>
2021-12-13 21:16:46 +0200

    Add files via upload

--- gameT.py
+++ gameT.py
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 import copy
 
 import numpy as np
@@ -17,23 +9,28 @@ import random
 rows_num = 6
 cols_num = 7
 
-agent_num = 1
-player_num = 2
+agent_num = 2
+player_num = 1
 
 connect4 = 4
 
 board = np.zeros((rows_num, cols_num))
+flatten_board = board.flatten()
+str_board = "".join(str(i) for i in flatten_board)
+set = {str_board: 1}
 
 
-class Node:
+#class Node:
 
-    def __init__(self,board,score):
-        self.children =[]
-        self.board=board
-        self.score=score
+#    def __init__(self, board, score):
+#        self.children = []
+#        self.board = board
+#        self.score = score
 
-    def add_child(self, child):
-        self.children.append(child)
+#    def add_child(self, child):
+#        self.children.append(child)
+#    def set_board(self, board):
+#        self.board = board
 
 
 def calc_h(connect):
@@ -47,7 +44,7 @@ def calc_h(connect):
         h += 5
     elif connect.count(player) == 4:
         h += 1000
-        
+
     if connect.count(opponent) == 2 and connect.count(0) == 2:
         h -= 2
     elif connect.count(opponent) == 3 and connect.count(0) == 1:
@@ -55,145 +52,177 @@ def calc_h(connect):
 
     return h
 
+
 def score_h(board):
     h = 0
 
     ## Score center
-    center_array = [int(i) for i in list(board[:, cols_num//2])]
+    center_array = [int(i) for i in list(board[:, cols_num // 2])]
     center_count = center_array.count(agent_num)
     h += center_count * 4
-    
+
     ## Score Horizontal
     for r in range(rows_num):
-        row_array = [int(i) for i in list(board[r,:])]
-        for c in range(cols_num-3):
-            connect = row_array[c:c+connect4]
+        row_array = [int(i) for i in list(board[r, :])]
+        for c in range(cols_num - 3):
+            connect = row_array[c:c + connect4]
             h += calc_h(connect)
     ## Score Vertical
     for c in range(cols_num):
-        col_array = [int(i) for i in list(board[:,c])]
-        for r in range(rows_num-3):
-            connect = col_array[r:r+connect4]
+        col_array = [int(i) for i in list(board[:, c])]
+        for r in range(rows_num - 3):
+            connect = col_array[r:r + connect4]
             h += calc_h(connect)
     ## Score diagonal
-    for r in range(rows_num-3):
-        for c in range(cols_num-3):
-            connect = [board[r+i][c+i] for i in range(connect4)]
+    for r in range(rows_num - 3):
+        for c in range(cols_num - 3):
+            connect = [board[r + i][c + i] for i in range(connect4)]
             h += calc_h(connect)
 
-    for r in range(rows_num-3):
-        for c in range(cols_num-3):
-            connect = [board[r+3-i][c+i] for i in range(connect4)]
+    for r in range(rows_num - 3):
+        for c in range(cols_num - 3):
+            connect = [board[r + 3 - i][c + i] for i in range(connect4)]
             h += calc_h(connect)
     return h
 
 
 def get_next_valid_slot(board, col):
-    for i in reversed(( range(rows_num))):
+    for i in reversed((range(rows_num))):
         if board[i][col] == 0:
             return i
 
 
-def get_children(node, maximizing_player):
-    board=node.board
-    children =[]
+def get_children(board, maximizing_player):
+   # board = node.current_board
+    children = []
     for col in range(cols_num):
         if board[0][col] == 0:
             child_board = copy.deepcopy(board)
             row = get_next_valid_slot(board, col)
-            if maximizing_player :
+            if maximizing_player:
                 child_board[row][col] = agent_num
-            else :
+            else:
                 child_board[row][col] = player_num
 
-            child_node =Node(child_board,score_h(child_board))
-            children.append(child_node)
+            #child_node = Node(child_board, score_h(child_board))
+            children.append(child_board)
     return children
 
 
-
-
 def is_terminal(board):
-
     x = np.count_nonzero(board == 0)
     return x == 0
 
-root =Node(board,score_h(board))
 
 
 
-def get_root_children():
-    children = get_children(root, True)
-    for child in children:
-        root.add_child(child)
-
-
-
-def min_max(node, depth, maximizing_player):
-
-    board=node.board
+def min_max_ab(board, depth,alpha, beta, maximizing_player):
+    #board = node.current_board
 
     if is_terminal(board) or depth == 0:
-        return score_h(board)
-
+        return score_h(board),None
     if maximizing_player:
         v = -math.inf
-        children = get_children(node,True)
+        children = get_children(board, True)
+        choosen_child = children[0]
         for child in children:
-            node.add_child(child)
-            v = max(v, min_max(child, depth - 1, False))
-            node.score =v
-        return v
+            #node.add_child(child)
+
+            value= min_max(child, depth - 1, False)[0]
+            if value > v :
+                choosen_child = child
+                v = value
+            #node.score = v
+            alpha = max(alpha, v)
+            if alpha >= beta:
+                break
+        return v,choosen_child
     else:
         v = math.inf
-        children = get_children(node,False)
+        children = get_children(board, False)
+        choosen_child = children[0]
         for child in children:
-            node.add_child(child)
-            v = min(v, min_max(child, depth - 1, True))
-            node.score =v
-        return v
+            #node.add_child(child)
+            value = min_max(child, depth - 1, True)[0]
+            if value < v :
+                choosen_child = child
+                v = value
+            beta = min(beta, v)
+            if alpha >= beta:
+                break
+            #node.score = v
+        return v,choosen_child
 
 
 
-def best_path(node,final_score):
-    path =[]
+
+
+def min_max(board, depth, maximizing_player):
+    # board = node.current_board
+
+    if is_terminal(board) or depth == 0:
+        return score_h(board), None
+    if maximizing_player:
+        v = -math.inf
+        children = get_children(board, True)
+        choosen_child = children[0]
+        for child in children:
+            # node.add_child(child)
+            flatten_board = child.flatten()
+            str_board = "".join(str(i) for i in flatten_board)
+            if not (str_board in set):
+                set[str_board] = 1
+                value = min_max(child, depth - 1, False)[0]
+                if value > v:
+                    choosen_child = child
+                    v = value
+                # no = {}de.score = v
+        return v, choosen_child
+    else:
+        v = math.inf
+        children = get_children(board, False)
+        choosen_child = children[0]
+        for child in children:
+            flatten_board = child.flatten()
+            str_board = "".join(str(i) for i in flatten_board)
+            if not (str_board in set):
+                set[str_board] = 1
+                # node.add_child(child)
+                value = min_max(child, depth - 1, True)[0]
+                if value < v:
+                    choosen_child = child
+                    v = value
+            # node.score = v
+        return v, choosen_child
+
+
+
+def best_path(node, final_score):
+    path = []
     path.append(node.board)
     current = node
-    depth =7
-    while not (is_terminal(current.board)) and depth > 0 :
-        for child in current.children :
-            if child.score == final_score :
+    depth = 7
+    print("hereu")
+
+    while (not (is_terminal(current.board))) and depth > 0:
+        for child in current.children:
+            if child.score == final_score:
                 path.append(child.board)
                 current = child
                 break
+    print("hered")
 
     return path
 
 
 
-get_root_children()
-final_score = min_max(root,7,True)
-new_board = copy.deepcopy(board)
-children = get_children(root, True)
-for child in children :
-    if child.score == final_score :
-        new_board = child.board
-        break
 
 
 
-new_root = Node(new_board,root.score)
-# number of branches in 1st level !!
-root.children.pop(0)
-root.children.pop(0)
 
-print(best_path(root,final_score))
-print(new_root.board)
 
-#get player move now
-#new_root
 
-while True:
-    pass
-    #min_max(new_root,7,True)
-    #player turn now
+
+
+
+
